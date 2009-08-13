83a783f721c7d89c875f2ccd8e59b3245d58c95b
Geoffrey Hing <ghing@ghing-mini.(none)>
2009-08-13 18:14:38 -0500

    Finished implementing string splitting method.

--- ctatwitter.py
+++ ctatwitter.py
@@ -16,13 +16,33 @@ class ShortMessage(object):
         self._msg = msg
 
     def split(self, max_length):
-        """Split the message at word boundaries into strings of max_length length.  Returns a list of strings of max_length or less."""
+        """Split the message at word boundaries into strings of max_length length.  Returns a list of strings of max_length or less. Note that this method replaces runs of whitespace with a single space."""
+        messages = []
+
         # TODO: Implement this method
         if len(self._msg) <= max_length:
             # Message is within the length limit.  Just return it.
-            return [self._msg]
+            messages.append(self._msg)
         else:
-            pass
+            msg_words = self._msg.split()
+            while len(msg_words) > 0:
+               message = ""
+               max_len_reached = False
+               while not max_len_reached:
+                   if len(message) + len(msg_words[0]) <= max_length:
+                       # Our message isn't full length yet
+                       message += msg_words.pop(0)
+
+                       if len(msg_words) > 0 and len(message) + 1 <= max_length:
+                           message += ' ' 
+                   else:
+                       max_len_reached = True
+
+               messages.append(message)
+
+        return messages
+
+
 
 # NOTE: This is the command syntax:
 # 
