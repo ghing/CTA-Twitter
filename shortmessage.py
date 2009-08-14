ba4155dc57105e927ff7aec9e77be8694ec5bd33
Geoffrey Hing <ghing@ghing-mini.(none)>
2009-08-14 08:14:27 -0500

    Broke ShortMessage out into it's own package to make testing easier since I don't have some of the dependencies installed on my network to just import ctatwitter.py into my test cases.

---
+++ shortmessage.py
@@ -0,0 +1,32 @@
+class ShortMessage(object):
+    """A class to represent short messages such as a tweet or SMS message"""
+
+    def __init__(self, msg):
+        self._msg = msg
+
+    def split(self, max_length):
+        """Split the message at word boundaries into strings of max_length length.  Returns a list of strings of max_length or less. Note that this method replaces runs of whitespace with a single space."""
+        messages = []
+
+        # TODO: Implement this method
+        if len(self._msg) <= max_length:
+            # Message is within the length limit.  Just return it.
+            messages.append(self._msg)
+        else:
+            msg_words = self._msg.split()
+            while len(msg_words) > 0:
+               message = ""
+               max_len_reached = False
+               while not max_len_reached:
+                   if len(message) + len(msg_words[0]) <= max_length:
+                       # Our message isn't full length yet
+                       message += msg_words.pop(0)
+
+                       if len(msg_words) > 0 and len(message) + 1 <= max_length:
+                           message += ' ' 
+                   else:
+                       max_len_reached = True
+
+               messages.append(message)
+
+        return messages
