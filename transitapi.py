b4dc05589b5faffd6b19617b89614994468199fa
1620 <u35733891@infong461.perfora.net>
2009-08-23 19:00:42 -0400

    Started implementing getStopPredictions()

--- transitapi.py
+++ transitapi.py
@@ -119,31 +119,26 @@ class Bustracker(object):
         return stops
 
     def parse_stop_predictions_xml(self, data):
+        # BOOKMARK
+        # TODO: Implement this
         pass
             
     def getStopPredictions(self, stop, route):
         # Example Request: http://chicago.transitapi.com/bustime/map/getStopPredictions.jsp?stop=8207&route=49
         # Notes: route can be stacked. ie: route=50-92. which would give you stop predictions for stops that have both foster and damen.
-        # Example Response: 
-        # <stop>
-        #     <id>8207</id>
-        #     <nm>Western & Fullerton</nm>
-        #     <sri>
-        #         <rt>49</rt>
-        #         <d>South Bound</d>
-        #     </sri>
-        #     <sbs>
-        #
-			  #     </sbs>
-        #     <cr>49</cr>
-        #     <pre>
-        #         <pt>DELAYED</pt>
-        #         <fd>79th</fd>
-        #         <v>6562</v>
-        #         <rn>49</rn>
-        #     </pre>
-        # </stop>
-        pass
+
+        url = "http://chicago.transitapi.com/bustime/map/getStopPredictions.jsp?stop=%s&route=%s" % (stop, route)
+        try:
+            data = urllib2.urlopen(url).read()
+        except urllib2.HTTPError, e:
+	    print "HTTP error: %d" % e.code
+        except urllib2.URLError, e:
+	    print "Network error: %s" % e.reason.args[1]
+
+        
+        buses = self.parse_stop_predictions(data)
+
+        return buses
 
 def main():
     bt = Bustracker()
