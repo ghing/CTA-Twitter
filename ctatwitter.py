580e13be42d9bf14e3b408214fed82bc0e1e30c6
1620 <u35733891@infong461.perfora.net>
2009-09-03 00:33:05 -0400

    Added AGPL messages.

--- ctatwitter.py
+++ ctatwitter.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# Copyright 2009 Geoffrey Hing
+#
+# This file is part of the CTA Twitter package.
+#
+# CTA Twitter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CTA Twitter is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU AfferoGeneral Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with CTA Twitter.  If not, see <http://www.gnu.org/licenses/>.
+
 import twitter
 import sqlite3
 import getopt, sys
--- transitapi.py
+++ transitapi.py
@@ -1,3 +1,20 @@
+# Copyright 2009 Geoffrey Hing
+#
+# This file is part of the CTA Twitter package.
+#
+# CTA Twitter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CTA Twitter is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU AfferoGeneral Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with CTA Twitter.  If not, see <http://www.gnu.org/licenses/>.
+
 import urllib2
 import xml.dom.minidom
 import xml.sax.saxutils
